eed59897516ee38db906abb8f18fc772689bcdeb
ren <ren443te@gmail.com>
2019-12-07 07:43:18 +0900

    controller2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,7 @@ class ArticlesController < ApplicationController
 
   def edit
   end
+
+  def new
+  end
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -23,4 +23,11 @@ RSpec.describe ArticlesController, :type => :controller do
     end
   end
 
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
